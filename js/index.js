2f485f9ad8b64e512ac8aeee15579995b199c914
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-11-08 01:29:23 +0200

    add more cats

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 var cats = [
-    {name: "Fizzy", src: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80"},
-    {name: "Lola", src: "https://rukminim1.flixcart.com/image/416/416/kph8h3k0/poster/e/w/4/large-adorable-cat-poster-cute-kittens-poster-cat-poster-funny-original-imag3p7tcxuzhpn2.jpeg?q=70"}
+    {
+        name: "Fizzy", 
+        src: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+        clickcount: "0"
+    },
+    {
+        name: "Lola", 
+        src: "https://rukminim1.flixcart.com/image/416/416/kph8h3k0/poster/e/w/4/large-adorable-cat-poster-cute-kittens-poster-cat-poster-funny-original-imag3p7tcxuzhpn2.jpeg?q=70",
+        clickCount: "0"
+    },
+    {
+        name: "Jynx", 
+        src: "https://play-lh.googleusercontent.com/AmKSpZt_rynhOO0ID1eS0gqeW3DFzoH6KNZkAAgepQ0t9MDRQTmil-nlY5GqkZ_7El0",
+        clickCount: "0"
+    },
+    {
+        name: "Roi", 
+        src: "https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg",
+        clickCount: "0"
+    },
+    {
+        name: "Fluff", 
+        src: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F06%2F26%2Ftiny-white-kitten-873941684-2000.jpg",
+        clickCount: "0"
+    }
 ]
 
 fn_increment=(elem, current)=>{
@@ -36,5 +59,5 @@ fn_make_cat_card = (cat) => {
 }
 
 $(document).ready(()=>{
-    cats.forEach((x)=> $('.container').append(fn_make_cat_card(x)));
+    cats.forEach((x)=> $('.content').append(fn_make_cat_card(x)));
 })
