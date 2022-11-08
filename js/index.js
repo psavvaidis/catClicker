72e5c866a80f7907809a56846464ddcc93394d4c
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-11-08 03:00:08 +0200

    Persist click counts

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var cats = [
     {
         name: "Fizzy", 
         src: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-        clickcount: "0"
+        clickCount: "0"
     },
     {
         name: "Lola", 
@@ -28,12 +28,14 @@ var cats = [
 
 fn_increment=(elem, current)=>{
     elem.html(current + 1);
+    return current + 1
+
 }
 
 fn_btn_click=(event)=>{
     var counter = $(event.target).siblings('.cat-card_counter-text').children('span');
     var currentCount = parseInt($(counter).html());
-    fn_increment(counter, currentCount);
+    event.data.cat.clickCount = fn_increment(counter, currentCount).toString();
 }
 
 fn_make_cat_card = (cat) => {
@@ -46,13 +48,13 @@ fn_make_cat_card = (cat) => {
     $(counter_text).addClass("cat-card_counter-text");
     $(image).addClass("cat-card_image");
     $(image).attr('src', cat.src);
-    $(image).click(fn_btn_click)
+    $(image).click({"cat": cat}, fn_btn_click)
     $(counter).addClass("cat-card_counter-text_counter")
     $(card).addClass("cat-card");
     $(name_text).addClass("cat-card_name");
 
     $(name_text).html(cat.name);
-    $(counter).html("0");
+    $(counter).html(cat.clickCount);
     $(counter_text).append(counter," clicks!");
     $(card).append(name_text, image, counter_text);
     return card
