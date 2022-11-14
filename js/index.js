a6606796b6bf9fef8a514fbdf00179e97cbe8cac
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-11-14 10:26:02 +0200

    Separate code on M,O and 2 views

--- js/index.js
+++ js/index.js
@@ -2,48 +2,85 @@ import { default as cats } from './data.js';
 import * as utils from './utilities.js';
 
 var model = {
-    init: (cats)=>{utils.save_locally(cats)},
-    get: (cat, catlist = JSON.parse(localStorage.cats))=>{ return catlist.filter((el)=> el.name == cat.name)},
-    get_all: (catlist=JSON.parse(localStorage.cats))=>{ return catlist},
+    init: (cats)=>{
+        utils.save_locally(cats)
+    },
+    get: (cat, catlist = JSON.parse(localStorage.cats))=>{ 
+        return catlist.filter((el)=> el.name == cat)
+    },
+    get_all: (catlist=JSON.parse(localStorage.cats))=>{return catlist},
     setCount: (count, cat, catlist = JSON.parse(localStorage.cats))=>{
         catlist.forEach(element => {
             if(element == cat){
                 element.clickCount = count;
             }
         });
+        utils.save_locally(catlist);
+    },
+    setName: (name, cat, catlist = JSON.parse(localStorage.cats))=>{
+        catlist.forEach(element => {
+            if(element == cat){
+                element.clickCount = count;
+            }
+        });
+        utils.save_locally(catlist);
+    },
+    setCount: (count, cat, catlist = JSON.parse(localStorage.cats))=>{
+        // model.get(cat.name,catlist)
+        catlist.forEach(element => {
+            if(element == cat){
+                element.clickCount = count;
+            }
+        });
+        utils.save_locally(catlist);
     }
 }
 
 var sidebar_view = {
-    init: ()=> {},
+    init: ()=> {
+        var cats = octopus.load_list();
+        $(".sidebar").html(utils.load_cat_list(cats));
+        utils.add_cat_list_event_listeners(cats, $(".cat-list").find("li"), octopus.updateCard);
+    },
     update: ()=> {}
 }
 
 var card_view = {
     init: ()=> {},
-    update: ()=> {}
+    update: (target)=> {
+        utils.load_cat(model.get($(target).attr("id"))[0], $('.content'));
+    }
 }
 
 var octopus = {
-    init: ()=> {
-        model.init();
+    init: (cats)=> {
+        model.init(cats);
         sidebar_view.init();
         card_view.init();
     },
-    updateCard: ()=>{},
-    incrementCatCount: ()=>{}
+    load_list: ()=>{ return model.get_all()},
+    updateCard: (target)=>{
+        card_view.update(target);
+    },
+    incrementCatCount: (event)=>{
+        var counter = $(event.target).siblings('.cat-card_counter-text').children('span');
+        var currentCount = parseInt($(counter).html());
+        event.data.cat.clickCount = utils.increment(counter, currentCount).toString();
+    }
 }
 
 $(document).ready(()=>{
     
     // Render cat list to the sidebar
-    console.log(cats)
-    $(".sidebar").html(utils.load_cat_list(cats));
+    
+    // $(".sidebar").html(utils.load_cat_list(cats));
 
     // add click listeners to sidebar cat names to show each cat
-    utils.add_cat_list_event_listeners(cats, $(".cat-list").find("li"));
+    // utils.add_cat_list_event_listeners(cats, $(".cat-list").find("li"));
     // cats.forEach((x)=> $('.content').append(fn_make_cat_card(x)));
 
     //GOAL
-    //octopus.init()
+    octopus.init(cats);
 })
+
+export {octopus};
